8ed688b327bfca2298986765de0d974c7e50f71b
Ku-Lung Hsu <kenhsu@wireless-10-154-170-170.public.utexas.edu>
2026-07-30 12:40:59 -0500

    Add admin panel, news-data.js, dynamic news rendering with 6-month auto-archive

---
+++ news-data.js
@@ -0,0 +1,94 @@
+// HSU LAB NEWS DATA
+// This file is auto-updated by the admin panel.
+// Do not edit manually.
+
+const HSU_LAB_NEWS = [
+  {
+    date: "2026-05",
+    items: [
+      "Congrats to Surya for receiving the Summer 2026 Chemistry Department Research Fellowship!",
+      "Best of luck to our former undergraduate researchers, Scout and Quentin, in their future endeavors. Happy graduation!",
+      "We enjoyed a nice <a href='photos.html'>celebration</a> for Olivia with the Hsu Lab crew this May!"
+    ]
+  },
+  {
+    date: "2026-04",
+    items: [
+      "Congrats to Dr. Olivia Murtagh on successfully defending her thesis in April!",
+      "Scout presented at the senior chemistry capstone poster session, great job Scout!"
+    ]
+  },
+  {
+    date: "2026-03",
+    items: [
+      "Congrats to John M. on passing his candidacy qualifying exam!"
+    ]
+  },
+  {
+    date: "2026-02",
+    items: [
+      "Congrats to Olivia on publishing her low-microflow targeted lipidomics method for femtomole scale quantification of tissue lipids in <a href='https://pubs.acs.org/doi/full/10.1021/acs.analchem.5c05829' target='_blank'>Analytical Chemistry</a>!",
+      "We thank UT Austin for letting us show hundreds of young students the fascinating world of lipid biochemistry at STEM Girl Day 2026! Check out some pictures from the event <a href='photos.html'>here</a>."
+    ]
+  },
+  {
+    date: "2026-01",
+    items: [
+      "Congrats to Madeleine and David for publishing in <em><a href='https://pubs.rsc.org/en/content/articlelanding/2026/sc/d5sc07398g' target='_blank'>Chemical Science</a></em> on their identification of a tyrosine-reactive inhibitor of the protein tyrosine phosphatase, SHP2!",
+      "Also go check out Justin's <a href='http://biorxiv.org/content/10.64898/2026.01.27.702106v1.abstract' target='_blank'>preprint publication in BioRxiv</a> on re-engineering P(V) chemical warfare electrophiles into novel chemical probes!"
+    ]
+  },
+  {
+    date: "2025-12",
+    items: [
+      "Congrats to Dr. David Leace on his successful doctoral defense. We had a great time <a href='photos.html'>celebrating him and the holidays together</a> this December."
+    ]
+  },
+  {
+    date: "2025-10",
+    items: [
+      "Congrats to Xiaoding on publishing a preprint of his paper on a covalent PFKL activator which shrinks tumors, check it out in <a href='https://pmc.ncbi.nlm.nih.gov/articles/PMC12621983/' target='_blank'>bioRxiv</a>!",
+      "4 new graduate students joined the Hsu lab family this fall: Alyssa Gomez, Cameron Muniz, John Thorup, and Tobias Jonson. Welcome!"
+    ]
+  },
+  {
+    date: "2025-09",
+    items: [
+      "Congrats to David on publishing in <a href='https://pubs.acs.org/doi/10.1021/jacs.5c06951' target='_blank'>JACS</a> on tyrosine reactive inhibitors which can function as molecular glues through ligand induced protein tethering (LIPT)!"
+    ]
+  },
+  {
+    date: "2025-08",
+    items: [
+      "Congrats to Dr. Michael Founds on successfully defending his thesis. We enjoyed a nice <a href='photos.html'>celebration</a> with the Hsu Lab crew!"
+    ]
+  },
+  {
+    date: "2025-06",
+    items: [
+      "Congrats to Olivia on presenting in Thermo Scientific's Metabolomics Breakfast Workshop at <a href='https://assets.thermofisher.com/TFS-Assets/CMD/brochures/bk-003744-ASMS-2025-bk003744-en-teaser.pdf' target='_blank'>ASMS 2025</a>!",
+      "Wishing Wes the best of luck at his internship with Talus Bioscience this summer working on transcription factor drug discovery!"
+    ]
+  },
+  {
+    date: "2025-05",
+    items: [
+      "Congrats to Michael on publishing on how PTGR2 inhibition alters inflammatory responses in macrophages in <a href='https://pubs.acs.org/doi/full/10.1021/acschembio.5c00231' target='_blank'>ACS Chem Bio</a>!",
+      "Congrats to Gibae Kim on his beyond cysteine covalent chemistry review in <a href='https://pubs.acs.org/doi/10.1021/acs.chemrev.5c00001' target='_blank'>Chemical Reviews</a>!"
+    ]
+  },
+  {
+    date: "2025-04",
+    items: [
+      "Congrats to Justin on publishing on the development of a selective covalent AKR1C3 inhibitor in the <a href='https://pubs.acs.org/doi/full/10.1021/acs.jmedchem.5c00050' target='_blank'>Journal of Medicinal Chemistry</a>.",
+      "Congrats to Mikayla, Payton, Surya, Wes, and Yen-Ting for all passing their candidacy qualifying exams!"
+    ]
+  },
+  {
+    date: "2025-02",
+    items: [
+      "Congrats to Alexis on her review on intact and native protein analysis using chemical proteomics in <a href='https://www.sciencedirect.com/science/article/pii/S0003267024013527?via%3Dihub' target='_blank'>Analytica Chimica Acta</a>!",
+      "Welcome to our new postdoctoral fellow, Zhuoheng Li!"
+    ]
+  }
+];
